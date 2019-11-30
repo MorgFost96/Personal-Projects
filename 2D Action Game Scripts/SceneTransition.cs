db9b55a4857f0bd06a773fddb973444c82040ee2
MorgFost96 <MorgFost96@users.noreply.github.com>
2019-11-30 01:10:10 -0800

    2D Action Game Script

---
+++ 2D Action Game Scripts/SceneTransition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour {
+
+    private Animator TransitionAnim;
+
+	private void Start ()
+    {
+        TransitionAnim = GetComponent<Animator>();	
+	}
+
+    public void LoadScene(string sceneName)
+    {
+        StartCoroutine(Transition(sceneName));
+    }
+
+    IEnumerator Transition(string sceneName)
+    {
+        TransitionAnim.SetTrigger("End");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(sceneName);
+    }
+}
